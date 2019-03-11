844ab20c3ccaaa2ed0e49165b3d4385c78182cd4
godcong <jumbycc@163.com>
2019-03-11 16:04:19 +0800

    update keygen

---
+++ go.mod
@@ -0,0 +1,5 @@
+module ipfs-keygen
+
+go 1.12
+
+require github.com/libp2p/go-libp2p-pnet v0.0.1
---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"flag"
+	"github.com/libp2p/go-libp2p-pnet"
+	"io"
+	"log"
+	"os"
+	"path/filepath"
+)
+
+var file = flag.String("file", "swarm.key", "set the key output path")
+
+func main() {
+	_ = os.MkdirAll(filepath.Dir(*file), os.ModePerm)
+	openFile, e := os.OpenFile(*file, os.O_CREATE|os.O_SYNC|os.O_RDWR, os.ModePerm)
+	if e != nil {
+		panic(e)
+	}
+	reader, e := pnet.GenerateV1PSK()
+	if e != nil {
+		panic(e)
+	}
+	i, e := io.Copy(openFile, reader)
+	if e != nil {
+		panic(e)
+	}
+	log.Println("success with bytes:", i)
+}
